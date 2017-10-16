d1e897ca8ebbcc25f4cfbc37b3e53c96587d4858
Kubota Naohiro <naohirok.1726@gmail.com>
2017-10-16 17:06:38 +0900

    end of Routing

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,6 +6,9 @@ import { HeroService } from './hero.service';
 @Component({
     selector: 'my-dashboard',
     templateUrl: './dashboard.component.html',
+    styleUrls: [
+        './dashboard.component.css',
+    ],
 })
 
 export class DashboardComponent implements OnInit {
